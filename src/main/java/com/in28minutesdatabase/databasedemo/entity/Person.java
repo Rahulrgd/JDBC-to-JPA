ab414e3434c481bba1289f90a99d19e735dd97a6
RahulGupta <rahulrgd21@gmail.com>
2024-02-14 21:34:16 +0530

    Step 14 - Defining Person Entity

--- src/main/java/com/in28minutesdatabase/databasedemo/entity/Person.java
+++ src/main/java/com/in28minutesdatabase/databasedemo/entity/Person.java
@@ -2,9 +2,21 @@ package com.in28minutesdatabase.databasedemo.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+// @Table(name = "person")
 public class Person {
 
+  @Id
+  @GeneratedValue
   private int id;
+
+  // @Column(name="name")
   private String name;
   private String location;
   private Date birthDate;
@@ -14,6 +26,14 @@ public class Person {
   public Person(int id, String name, String location, Date birthDate) {
     super();
     this.id = id;
+    this.name = name;
+    this.location = location;
+    this.birthDate = birthDate;
+  }
+
+  // Constructor without Id 
+  public Person(String name, String location, Date birthDate) {
+    super();
     this.name = name;
     this.location = location;
     this.birthDate = birthDate;
