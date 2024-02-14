fe6be03b74530da1d33944430be89c7ea5820e46
RahulGupta <rahulrgd21@gmail.com>
2024-02-14 21:01:56 +0530

    Step 11 - Implementing insert and update Spring JDBC Update Methods

--- src/main/java/com/in28minutesdatabase/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28minutesdatabase/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,7 @@
 package com.in28minutesdatabase.databasedemo;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.in28minutesdatabase.databasedemo.entity.Person;
 import com.in28minutesdatabase.databasedemo.jdbc.PersonJdbcDAO;
 
 @SpringBootApplication
@@ -26,6 +29,8 @@ public class DatabaseDemoApplication implements CommandLineRunner{
 		logger.info("All user -> {}",  dao.findAll());
 		logger.info("\nUser id 10001 -> {}",  dao.findById(10001));
 		logger.info("\nDeleting User 10001 and number of rows affected -> {}",  dao.deleteById(10001));
+		logger.info("\nInsert new User -> {}",  dao.insert(new Person(10004, "Shubham", "Gujrat", new Date())));
+		logger.info("\nUpdate new User -> {}",  dao.update(new Person(10002, "Saurabh", "Bhopal", new Date())));
 	}
 
 }
